45cfd93ab61d27a02479d7ac4fa7c3dde05a9e74
erleibiazzio <erleibiazzio@gmail.com>
2022-05-25 08:51:25 -0300

    Atualiza link dos brasões de Ipirá e Itá Ref.: #25

--- themes/AMAUC/layouts/parts/footer.php
+++ themes/AMAUC/layouts/parts/footer.php
@@ -7,10 +7,10 @@
                 <a href="https://altobelavista.sc.gov.br" title="Município de Alto da Bela Vista"><img src="<?php $this->asset('img/municipios/alto-bela-vista.png'); ?>" alt="Município de Alto da Bela Vista"></a>
                 <a href="https://arabuta.atende.net/" title="Município de Arabutã"><img src="<?php $this->asset('img/municipios/arabuta.png'); ?>" alt="Município de Arabutã"></a>
                 <a href="https://concordia.atende.net/" title="Município de Concórdia"><img src="<?php $this->asset('img/municipios/concordia.png'); ?>" alt="Município de Concórdia"></a>
-                <a href="https://ipira.ba.gov.br/" title="Município de Ipira"><img src="<?php $this->asset('img/municipios/ipira.png'); ?>" alt="Município de Ipira"></a>
+                <a href="https://ipira.sc.gov.br/" title="Município de Ipira"><img src="<?php $this->asset('img/municipios/ipira.png'); ?>" alt="Município de Ipira"></a>
                 <a href="https://ipumirim.sc.gov.br/" title="Município de Ipumirim"><img src="<?php $this->asset('img/municipios/ipumirim.png'); ?>" alt="Município de Ipumirim"></a>
                 <a href="https://irani.sc.gov.br/" title="Município de Iraní"><img src="<?php $this->asset('img/municipios/irani.png'); ?>" alt="Município de Iraní"></a>
-                <a href="https://ita.sc.gov.br/" title="Município de Itá"><img src="<?php $this->asset('img/municipios/ita.png'); ?>" alt="Município de Itá"></a>
+                <a href="https://ita.atende.net/cidadao" title="Município de Itá"><img src="<?php $this->asset('img/municipios/ita.png'); ?>" alt="Município de Itá"></a>
                 <a href="http://www.jabora.sc.gov.br/" title="Município de Jaborá"><img src="<?php $this->asset('img/municipios/jabora.png'); ?>" alt="Município de Jaborá"></a>
                 <a href="https://lindoiadosul.sc.gov.br/" title="Município de Lindóia do Sul"><img src="<?php $this->asset('img/municipios/lindoia-do-sul.png'); ?>" alt="Município de Lindóia do Sul"></a>
                 <a href="https://peritiba.sc.gov.br/" title="Município de Peritiba"><img src="<?php $this->asset('img/municipios/peritiba.png'); ?>" alt="Município de Peritiba"></a>
