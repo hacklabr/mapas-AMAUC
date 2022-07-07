9b78f5539bff87457dd6cd00b7e84515747ad045
erleibiazzio <erleibiazzio@gmail.com>
2022-07-07 15:43:58 -0300

    Configura o termo de usu de imagem Ref.: #27

--- compose/common/config.d/lgpd.php
+++ compose/common/config.d/lgpd.php
@@ -8,6 +8,10 @@ return [
         'privacyPolicy' => [
             'title'=>  'Política de Privacidade do Mapa Cultural',
             'text'=> file_get_contents(__DIR__ . '/../lgpd-terms/privacy-policy.html')
-        ],        
+        ],
+        'imageOfUsage' => [
+            'title'=>  'Autorização de uso de imagem',
+            'text'=> file_get_contents(__DIR__ . '/../lgpd-terms/image-of-usage.html')
+        ],         
     ]
 ];
