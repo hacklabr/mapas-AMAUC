83081444b474dc9d094f695b020f0dcd1e2f2e2b
erleibiazzio <erleibiazzio@gmail.com>
2022-07-13 17:26:01 -0300

    Configura chat zammad para suporte Ref.: #26

---
+++ compose/common/config.d/zammad.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'zammad_src_form' => env('ZA_ZAMMAD_SRC_FORM', 'https://suporte.amauc.mapasculturais.com.br/assets/form/form.js'),
+    'zammad_src_chat' => env('ZA_ZAMMAD_SRC_CHAT', 'https://suporte.amauc.mapasculturais.com.br/assets/chat/chat.min.js'),
+    'zammad_enable' => env('ZA_ZAMMAD_ENABLE', true),
+    'zammad_background_color' => env('ZA_ZAMMAD_SRC_CHAT', '#157930'),
+];
