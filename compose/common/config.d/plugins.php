2e93017eaafd82488951623f14d81581ef44fbb6
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2023-10-02 22:51:35 -0300

    remove plugins não mais utilizados

--- compose/common/config.d/plugins.php
+++ compose/common/config.d/plugins.php
@@ -2,12 +2,7 @@
 
 return [
     'plugins' => [
-        'EvaluationMethodTechnical' => ['namespace' => 'EvaluationMethodTechnical'],
-        'EvaluationMethodSimple' => ['namespace' => 'EvaluationMethodSimple'],
-        'EvaluationMethodDocumentary' => ['namespace' => 'EvaluationMethodDocumentary'],
-        
         'MultipleLocalAuth' => [ 'namespace' => 'MultipleLocalAuth' ],
-        'SamplePlugin' => ['namespace' => 'SamplePlugin'],
         'MapasBlame' => [
             'namespace' => 'MapasBlame',
             'config' => [
@@ -15,14 +10,6 @@ return [
                     return $data;
                 },
             ]
-        ],
-        "LocationPatch" => [
-            "namespace" => "LocationPatch",
-            "config" => [
-                "enable" => env("LOCATION_PATCH_ENABLE", true),
-                "cutoff" => env("LOCATION_PATCH_CUTOFF", "19800101000001"),
-            ],
-        ],
-
+        ]
     ]
 ];
