c8e1b7463b334536adcc4858c3656ebcafc0da7d
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2023-10-02 23:17:21 -0300

    iniciando novo tema BaseV2

---
+++ themes/MapasAMAUC/Theme.php
@@ -0,0 +1,23 @@
+<?php
+namespace MapasAMAUC;
+
+use MapasCulturais\App;
+
+
+class Theme extends \MapasCulturais\Themes\BaseV2\Theme {
+ 
+    static function getThemeFolder() {
+        return __DIR__;
+    }
+
+    function _init() {
+        parent::_init();
+
+        $app = App::i();
+
+        $this->enqueueStyle("app-v2", "logo-footer", "css/logo-footer.css");
+        $app->hook("template(<<*>>.<<*>>.main-footer-links):after", function(){
+            $this->part("logo-footer");
+        });
+    }
+}
---
+++ themes/MapasAMAUC/layouts/parts/logo-footer.php
@@ -0,0 +1,16 @@
+<div class="logo-footer">
+    <a href="https://altobelavista.sc.gov.br" title="Município de Alto da Bela Vista"><img src="<?php $this->asset('img/municipios/alto-bela-vista.png'); ?>" alt="Município de Alto da Bela Vista"></a>
+    <a href="https://arabuta.atende.net/" title="Município de Arabutã"><img src="<?php $this->asset('img/municipios/arabuta.png'); ?>" alt="Município de Arabutã"></a>
+    <a href="https://concordia.atende.net/" title="Município de Concórdia"><img src="<?php $this->asset('img/municipios/concordia.png'); ?>" alt="Município de Concórdia"></a>
+    <a href="https://ipira.sc.gov.br/" title="Município de Ipira"><img src="<?php $this->asset('img/municipios/ipira.png'); ?>" alt="Município de Ipira"></a>
+    <a href="https://ipumirim.sc.gov.br/" title="Município de Ipumirim"><img src="<?php $this->asset('img/municipios/ipumirim.png'); ?>" alt="Município de Ipumirim"></a>
+    <a href="https://irani.sc.gov.br/" title="Município de Iraní"><img src="<?php $this->asset('img/municipios/irani.png'); ?>" alt="Município de Iraní"></a>
+    <a href="https://ita.atende.net/cidadao" title="Município de Itá"><img src="<?php $this->asset('img/municipios/ita.png'); ?>" alt="Município de Itá"></a>
+    <a href="http://www.jabora.sc.gov.br/" title="Município de Jaborá"><img src="<?php $this->asset('img/municipios/jabora.png'); ?>" alt="Município de Jaborá"></a>
+    <a href="https://lindoiadosul.sc.gov.br/" title="Município de Lindóia do Sul"><img src="<?php $this->asset('img/municipios/lindoia-do-sul.png'); ?>" alt="Município de Lindóia do Sul"></a>
+    <a href="https://peritiba.sc.gov.br/" title="Município de Peritiba"><img src="<?php $this->asset('img/municipios/peritiba.png'); ?>" alt="Município de Peritiba"></a>
+    <a href="https://www.piratuba.sc.gov.br/" title="Município de Piratuba"><img src="<?php $this->asset('img/municipios/piratuba.png'); ?>" alt="Município de Piratuba"></a>
+    <a href="https://castellobranco.sc.gov.br/" title="Município de Presidente Castelo Branco"><img src="<?php $this->asset('img/municipios/presidente-castello-branco.png'); ?>" alt="Município de Presidente Castelo Branco"></a>
+    <a href="https://www.seara.sc.gov.br/" title="Município de Seara"><img src="<?php $this->asset('img/municipios/seara.png'); ?>" alt="Município de Seara"></a>
+    <a href="https://xavantina.sc.gov.br/" title="Município de Xavantina"><img src="<?php $this->asset('img/municipios/xavantina.png'); ?>" alt="Município de Xavantina"></a>
+</div>
